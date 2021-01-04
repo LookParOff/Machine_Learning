af0c651229fc281fc706054801b24d3180830f86
PaRoNoIk <20956901+PaRoNoIk@users.noreply.github.com>
2021-01-04 16:18:09 +0300

    coded k-means clustering with nDim vectors

---
+++ k-means Claster.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def k_means(vectors: np.array, countOfClusters, distance=lambda x, y: abs(x - y)):
+    if type(vectors[0]) != np.ndarray:
+        DIM = 1
+    else:
+        DIM = len(vectors[0])
+    centroids = np.random.random((countOfClusters, DIM)) * vectors.max()
+    nearestClusters = [0 for _ in range(len(vectors))]  # nearest cluster for each vector
+    while True:
+        for index, vec in enumerate(vectors):
+            distsToClusters = [distance(vec, center) for center in centroids]
+            nearest = distsToClusters.index(min(distsToClusters))
+            nearestClusters[index] = nearest
+        newCentroids = centroids.copy() * 0
+        for index, indexOfNearestCluster in enumerate(nearestClusters):
+            newCentroids[indexOfNearestCluster] += vectors[index]
+        for i in range(countOfClusters):
+            c = nearestClusters.count(i)
+            if c == 0:
+                newCentroids[i] = centroids[i].copy()
+            else:
+                newCentroids[i] /= c
+        if abs((centroids - newCentroids).sum()) < 0.1*DIM:  # diff of all coord
+            break
+        centroids = newCentroids.copy()
+    return centroids
+
+
+# print(k_means(np.array([[1, 1], [2, 2], [5, 5], [6, 6]]), 2, lambda x, y: ((x[0] - y[0])**2 + (x[1] - y[1])**2)**0.5))
+# print(k_means(np.array([1, 1.1, 1.2, 50, 50.1, 50.2]), 2))
