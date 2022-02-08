9859d21e6a8022c07e583d5652db5a2e05b479d1
PaRoNoIk <20956901+PaRoNoIk@users.noreply.github.com>
2022-02-08 21:26:42 +0300

    Added visualisation of weights with heatmap. It's help to see a different between evolution models and SGD models

--- Tasks/Image_recognition/MLP_classic.py
+++ Tasks/Image_recognition/MLP_classic.py
@@ -3,21 +3,23 @@ import pandas as pd
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 from torchmetrics import Accuracy
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-class OrdinaryNet(torch.nn.Module):
+class MLP(torch.nn.Module):
     def __init__(self, count_of_inp_neu):
         super().__init__()
         self.input_shape = count_of_inp_neu
         self.relu = torch.nn.ReLU()
         self.softmax = torch.nn.Softmax()
 
-        self.linear1 = torch.nn.Linear(self.input_shape, 50)
-        self.linear2 = torch.nn.Linear(50, 10)
+        self.linear1 = torch.nn.Linear(self.input_shape, 10, device=device)
+        # self.linear2 = torch.nn.Linear(50, 10)
 
     def forward(self, x):
-        x = self.relu(self.linear1(x))
-        x = self.softmax(self.linear2(x))
+        # x = self.relu(self.linear1(x))
+        x = self.softmax(self.linear1(x))
         return x
 
 
@@ -48,11 +50,7 @@ def get_data_loaders(x, y):
 def fit(model, epochs, loss_func, opt, train_dl, test_dl):
     start_time = time()
     accuracy = Accuracy().to(device)
-    x, y = train_dl.dataset.tensors[0], train_dl.dataset.tensors[1]
-    y_predict = model(x)
-    loss = loss_func(y_predict, y)
-    acc = accuracy(y_predict, y)
-    print(f"Without train loss={round(loss.item(), 2)}; accuracy={round(acc.item(), 2)}")
+    x_test, y_test = test_dl.dataset.tensors[0], test_dl.dataset.tensors[1]
     for epoch in range(epochs):
         for x, y in train_dl:
             y_predict = model(x)
@@ -60,12 +58,29 @@ def fit(model, epochs, loss_func, opt, train_dl, test_dl):
             opt.zero_grad()
             loss.backward()
             opt.step()
-        if epoch % 5 == 0 or epoch < 5:
-            print(f"Epoch {epoch} calc in {round(time() - start_time)}sec and loss = {loss.item()}")
+        if epoch % 10 == 0:
+            y_predict = model(x_test)
+            acc = round(accuracy(y_predict, y_test).item(), 3)
+            print(f"Epoch {epoch} calc in {round(time() - start_time)}sec. "
+                  f"Loss = {loss.item()}. Test acc = {acc}")
             start_time = time()
-    x, y = test_dl.dataset.tensors[0], test_dl.dataset.tensors[1]
-    y_predict = model(x)
-    acc = accuracy(y_predict, y)
+            m = model.linear1.weight.detach().cpu().numpy()
+            fig, ax = plt.subplots(3, 3)
+            row, col = 0, 0
+
+            for i in range(9):
+                sns.heatmap(m[i, :].reshape((28, 28)),
+                            ax=ax[row, col])
+                col += 1
+                if col == 3:
+                    col = 0
+                    row += 1
+            plt.title(f"epoch №{epoch}", fontsize=20)
+            plt.tight_layout()
+            plt.show()
+
+    y_predict = model(x_test)
+    acc = accuracy(y_predict, y_test)
     print(f"Result accuracy={round(acc.item(), 3)}")
     return model
 
@@ -79,14 +94,14 @@ def main():
     input_shape = x.shape[1]
     train_dl, test_dl = get_data_loaders(x, y)
 
-    net = OrdinaryNet(input_shape)
+    net = MLP(input_shape)
     net = net.to(device=device, dtype=torch.float32)
     loss_fn = torch.nn.CrossEntropyLoss(reduction="sum")
 
     print("START TRAINING!")
     learning_rate = 0.001
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
-    epochs = 7
+    epochs = 70 + 1
     net = fit(net, epochs, loss_fn, optimizer, train_dl, test_dl)
     return net
 
@@ -94,11 +109,11 @@ def main():
 if __name__ == "__main__":
     device = torch.device("cuda")
     model = main()
-    path = r"C:\Users\Norma\PycharmProjects\Machine Learning\Datasets\MNIST\test.csv"
-    df = pd.read_csv(path, sep=",")
-    test_x = torch.tensor(df.to_numpy(), dtype=torch.float32, device=device)
-    res_test = model(test_x)
-    res_test = torch.argmax(res_test, dim=1)
-    df_res_test = pd.DataFrame(res_test.detach().cpu().numpy())
-    df_res_test.iloc[:, 0] += 1
-    df_res_test.to_csv(r"D:\res_mnist.csv", header=["ImageId", "Label"], index=False)
+    # path = r"C:\Users\Norma\PycharmProjects\Machine Learning\Datasets\MNIST\test.csv"
+    # df = pd.read_csv(path, sep=",")
+    # test_x = torch.tensor(df.to_numpy(), dtype=torch.float32, device=device)
+    # res_test = model(test_x)
+    # res_test = torch.argmax(res_test, dim=1)
+    # df_res_test = pd.DataFrame(res_test.detach().cpu().numpy())
+    # df_res_test.iloc[:, 0] += 1
+    # df_res_test.to_csv(r"D:\res_mnist.csv", header=["ImageId", "Label"], index=False)
--- Tasks/Image_recognition/MLP_evolution.py
+++ Tasks/Image_recognition/MLP_evolution.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import TensorDataset, DataLoader
-from torchmetrics import Accuracy
-# https://towardsdatascience.com/evolving-neural-networks-b24517bb3701
+from torchmetrics import Accuracy, ConfusionMatrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-class EvolutionNet:
+class MLP(torch.nn.Module):
     def __init__(self, count_of_inp_neu, param):
+        super().__init__()
         self.input = count_of_inp_neu
         self.mean, self.std = param
         self.hidden = 50
@@ -16,42 +18,60 @@ class EvolutionNet:
         self.relu = torch.nn.ReLU()
         self.sigmoid = torch.nn.Sigmoid()
         self.softmax = torch.nn.Softmax(dim=1)
-        self.linear1 = torch.zeros((self.input, self.hidden), device=device)
-        self.linear2 = torch.zeros((self.hidden, self.output), device=device)
-
-    def mutate(self):
-        self.linear1 += torch.normal(mean=self.mean, std=self.std,
-                                     size=self.linear1.shape, device=device)
-        self.linear2 += torch.normal(mean=self.mean, std=self.std,
-                                     size=self.linear2.shape, device=device)
+        self.linear1 = torch.nn.Linear(self.input, 10, device=device)
+        # self.linear2 = torch.zeros((self.hidden, self.output), device=device)
 
     def __call__(self, x):
-        x = self.relu(torch.mm(x, self.linear1))
-        x = self.softmax(torch.mm(x, self.linear2))
+        x = self.softmax(self.linear1(x))
+        # x = self.softmax(torch.mm(x, self.linear2))
         return x
 
+    def mutate(self):
+        self.linear1.weight += torch.normal(mean=self.mean, std=self.std,
+                                            size=self.linear1.weight.shape, device=device)
+        # self.linear2.weight += torch.normal(mean=self.mean, std=self.std,
+        #                                     size=self.linear2.weight.shape, device=device)
+
     def clone(self):
-        copy_net = EvolutionNet(self.input, (self.mean, self.std))
-        copy_net.linear1 = self.linear1.clone()
-        copy_net.linear2 = self.linear2.clone()
+        copy_net = MLP(self.input, (self.mean, self.std))
+        copy_net.load_state_dict(self.state_dict())
         return copy_net
+
+
+class NetEnsemble:
+    def __init__(self, count_of_inp_neu, param):
+        self.input = count_of_inp_neu
+        self.mean, self.std = param
+        self.relu = torch.nn.ReLU()
+        self.sigmoid = torch.nn.Sigmoid()
+        self.softmax = torch.nn.Softmax(dim=1)
+        self.linear1 = torch.zeros((self.input, 10), device=device)
 
 
 def parse_data(path):
     df = pd.read_csv(path, sep=",")
     y = df["label"]
     x = df.drop(columns="label")
-    x = torch.tensor(x.to_numpy(), dtype=torch.float32)
-    y = torch.tensor(y.to_numpy())
+    x = x.to_numpy()
+    y = y.to_numpy()
     return x, y
 
 
 def get_data_loaders(x, y):
     percentOfSplit = 0.10
+
     x_train = x[:x.shape[0] - int(x.shape[0] * percentOfSplit), :]
     y_train = y[:y.shape[0] - int(y.shape[0] * percentOfSplit)]
     x_test = x[x.shape[0] - int(x.shape[0] * percentOfSplit):, :]
     y_test = y[y.shape[0] - int(y.shape[0] * percentOfSplit):]
+
+    x_train = np.concatenate([x_train, x_train + np.random.normal(0, 0.3, size=x_train.shape)])
+    y_train = np.concatenate([y_train, y_train])
+
+    x_train = torch.tensor(x_train, dtype=torch.float32, device=device)
+    y_train = torch.tensor(y_train, device=device)
+    x_test = torch.tensor(x_test, dtype=torch.float32, device=device)
+    y_test = torch.tensor(y_test, device=device)
 
     train_dataset = TensorDataset(x_train, y_train)
     test_dataset = TensorDataset(x_test, y_test)
@@ -75,7 +95,8 @@ def fit(population: np.array, epochs, loss_func, train_dl, test_dl, descending=F
                 y_predict = specie(x)
                 loss = loss_func(y_predict, y)
                 result[id_specie] = loss
-            best_ids = torch.argsort(result, descending=descending)[:num_of_bests]  # chosen the bests
+            best_ids = torch.argsort(result, descending=descending)[
+                       :num_of_bests]  # chosen the bests
             best_species = population[best_ids]
             for id_specie, specie in enumerate(population):
                 population[id_specie] = best_species[id_specie % num_of_bests].clone()
@@ -88,6 +109,18 @@ def fit(population: np.array, epochs, loss_func, train_dl, test_dl, descending=F
             print(f"№{epoch} end in {round(time() - start)}secs. "
                   f"Loss = {round(torch.min(result).item(), 4)}. "
                   f"Acc = {acc}")
+            fig, ax = plt.subplots(3, 3)
+            row, col = 0, 0
+            m = best_species[0].linear1.weight.detach().cpu().numpy()
+            for i in range(9):
+                sns.heatmap(m[i, :].reshape((28, 28)), ax=ax[row, col])
+                col += 1
+                if col == 3:
+                    col = 0
+                    row += 1
+            plt.title(f"epoch №{epoch}", fontsize=20)
+            plt.tight_layout()
+            plt.show()
             start = time()
     return best_species
 
@@ -95,21 +128,21 @@ def fit(population: np.array, epochs, loss_func, train_dl, test_dl, descending=F
 def main():
     path = r"C:\Users\Norma\PycharmProjects\Machine Learning\Datasets\MNIST\train.csv"
     x, y = parse_data(path)
-    x, y = x.to(device), y.to(device)
-    x = x / torch.std(x)
-    x = x - torch.mean(x)
+    x = x / np.std(x)
+    x = x - np.mean(x)
     input_shape = x.shape[1]
     train_dl, test_dl = get_data_loaders(x, y)
 
     # loss_fn = Accuracy().to(device)
     loss_fn = torch.nn.CrossEntropyLoss()
     print("START TRAINING!")
-    epochs = 100 + 1
+    epochs = 1 + 1
     len_of_population = 20
     mutation_parameters = (0, 0.001)  # mean and std
-    population = np.array([EvolutionNet(input_shape, mutation_parameters)
+    population = np.array([MLP(input_shape, mutation_parameters)
                            for _ in range(len_of_population)])
-    arr_of_nets = fit(population, epochs, loss_fn, train_dl, test_dl)
+    with torch.no_grad():
+        arr_of_nets = fit(population, epochs, loss_fn, train_dl, test_dl)
 
     fitted_net = arr_of_nets[0]
     x, y = test_dl.dataset.tensors[0], test_dl.dataset.tensors[1]
@@ -117,12 +150,26 @@ def main():
     accuracy = Accuracy().to(device)
     acc = accuracy(y_predict, y)
     print(f"Result accuracy={round(acc.item(), 3)}")
+    conf_mat = ConfusionMatrix(10).to(device)
+    conf_mat(y_predict, test_dl.dataset.tensors[1])
     return arr_of_nets
 
 
 if __name__ == "__main__":
-    # todo появилась идея уменьшать все веса в 10 раз после длительной тренировки, таким образом
-    # todo случайный разброс уменьшится почти в 0, а то что не случайно останется +-
+    # https://towardsdatascience.com/evolving-neural-networks-b24517bb3701
     device = torch.device("cuda")
     # device = torch.device("cpu")
     model = main()
+
+    fig, ax = plt.subplots(3, 3)
+    row, col = 0, 0
+    for i in range(9):
+        sns.heatmap(model[0].linear1.weight.detach().cpu().numpy()[i, :].reshape((28, 28)),
+                    ax=ax[row, col])
+        col += 1
+        if col == 3:
+            col = 0
+            row += 1
+    plt.title("END", fontsize=20)
+    plt.tight_layout()
+    plt.show()
