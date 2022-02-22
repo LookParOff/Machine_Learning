f79f55495665f20767c0462128e54ae4501b5da7
PaRoNoIk <20956901+PaRoNoIk@users.noreply.github.com>
2022-02-22 17:50:46 +0300

    Now plot everything with plotly. Which mean it is beautiful :)

--- Tasks/Image_recognition/MLP_evolution.py
+++ Tasks/Image_recognition/MLP_evolution.py
@@ -1,51 +1,57 @@
 from time import time
 import pandas as pd
 import numpy as np
+
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 from torchmetrics import Accuracy, ConfusionMatrix
-import seaborn as sns
-import matplotlib.pyplot as plt
+
+import plotly.io as pio
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 class MLP(torch.nn.Module):
-    def __init__(self, count_of_inp_neu, param):
+    def __init__(self, count_of_inp_neu, count_of_out_neu):
         super().__init__()
         self.input = count_of_inp_neu
-        self.mean, self.std = param
-        self.hidden = 50
-        self.output = 10
-        self.relu = torch.nn.ReLU()
-        self.sigmoid = torch.nn.Sigmoid()
-        self.softmax = torch.nn.Softmax(dim=1)
-        self.linear1 = torch.nn.Linear(self.input, 10, device=device)
+        self.output = count_of_out_neu
+        self.linear1 = torch.nn.Linear(self.input, self.output, device=device)
         # self.linear2 = torch.zeros((self.hidden, self.output), device=device)
 
     def __call__(self, x):
-        x = self.softmax(self.linear1(x))
+        x = softmax(self.linear1(x))
         # x = self.softmax(torch.mm(x, self.linear2))
         return x
 
-    def mutate(self):
-        self.linear1.weight += torch.normal(mean=self.mean, std=self.std,
+    def mutate(self, mean, std):
+        self.linear1.weight += torch.normal(mean=mean, std=std,
                                             size=self.linear1.weight.shape, device=device)
         # self.linear2.weight += torch.normal(mean=self.mean, std=self.std,
         #                                     size=self.linear2.weight.shape, device=device)
 
     def clone(self):
-        copy_net = MLP(self.input, (self.mean, self.std))
+        copy_net = MLP(self.input, self.output)
         copy_net.load_state_dict(self.state_dict())
         return copy_net
 
 
-class NetEnsemble:
-    def __init__(self, count_of_inp_neu, param):
-        self.input = count_of_inp_neu
-        self.mean, self.std = param
-        self.relu = torch.nn.ReLU()
-        self.sigmoid = torch.nn.Sigmoid()
-        self.softmax = torch.nn.Softmax(dim=1)
-        self.linear1 = torch.zeros((self.input, 10), device=device)
+def get_gauss_kernel(size, sigma):
+    center = size // 2
+    kernel = np.zeros((size, size))
+    for i in range(size):
+        for j in range(size):
+            diff = np.sqrt((i - center) ** 2 + (j - center) ** 2)
+            kernel[i, j] = np.exp(-(diff ** 2) / (2 * sigma ** 2))
+    kernel /= np.sum(kernel)
+    return kernel
+
+
+def convolution2d(a, f):
+    s = f.shape + tuple(np.subtract(a.shape, f.shape) + 1)
+    strd = np.lib.stride_tricks.as_strided
+    subM = strd(a, shape=s, strides=a.strides * 2)
+    return np.einsum('ij,ijkl->kl', f, subM)
 
 
 def parse_data(path):
@@ -81,9 +87,11 @@ def get_data_loaders(x, y):
     return train_data_loader, test_data_loader
 
 
-def fit(population: np.array, epochs, loss_func, train_dl, test_dl, descending=False):
+def fit(population: np.array, epochs, parameters, loss_func, train_dl, test_dl, descending=False):
+    global fig
     """if we need minimize loss_func- descending should be True"""
     # how many of the best species do we take from the population
+    mean, std = parameters
     num_of_bests = int(len(population) * 0.1)
     best_species = None  # the best species of the population
     result = torch.zeros(len(population), dtype=torch.float32)
@@ -95,13 +103,29 @@ def fit(population: np.array, epochs, loss_func, train_dl, test_dl, descending=F
                 y_predict = specie(x)
                 loss = loss_func(y_predict, y)
                 result[id_specie] = loss
-            best_ids = torch.argsort(result, descending=descending)[
-                       :num_of_bests]  # chosen the bests
-            best_species = population[best_ids]
+            # choose the bests
+            best_ids = torch.argsort(result, descending=descending)
+            best_species = population[best_ids[:num_of_bests]]
+            result = result[best_ids]
             for id_specie, specie in enumerate(population):
                 population[id_specie] = best_species[id_specie % num_of_bests].clone()
                 if id_specie > num_of_bests:
-                    population[id_specie].mutate()
+                    population[id_specie].mutate(mean, std)
+
+        # save loss
+        history_of_fittness.append(result[0].item())
+        # plotting graphs
+        fig.add_trace(go.Scatter(x=list(range(len(history_of_fittness))), y=history_of_fittness),
+                      row=4, col=1)
+        row, col = 1, 1
+        w = population[0].linear1.weight.detach().cpu().numpy()  # weight
+        for i in range(9):
+            fig.add_heatmap(visible=False,
+                            z=w[i, :].reshape((28, 28)), row=row, col=col)
+            col += 1
+            if col == 4:
+                col = 1
+                row += 1
         if epoch % 10 == 0:
             x_test, y_test = test_dl.dataset.tensors[0], test_dl.dataset.tensors[1]
             y_predict = best_species[0](x_test)
@@ -109,18 +133,6 @@ def fit(population: np.array, epochs, loss_func, train_dl, test_dl, descending=F
             print(f"№{epoch} end in {round(time() - start)}secs. "
                   f"Loss = {round(torch.min(result).item(), 4)}. "
                   f"Acc = {acc}")
-            fig, ax = plt.subplots(3, 3)
-            row, col = 0, 0
-            m = best_species[0].linear1.weight.detach().cpu().numpy()
-            for i in range(9):
-                sns.heatmap(m[i, :].reshape((28, 28)), ax=ax[row, col])
-                col += 1
-                if col == 3:
-                    col = 0
-                    row += 1
-            plt.title(f"epoch №{epoch}", fontsize=20)
-            plt.tight_layout()
-            plt.show()
             start = time()
     return best_species
 
@@ -136,13 +148,13 @@ def main():
     # loss_fn = Accuracy().to(device)
     loss_fn = torch.nn.CrossEntropyLoss()
     print("START TRAINING!")
-    epochs = 1 + 1
+    epochs = 10 + 1
     len_of_population = 20
     mutation_parameters = (0, 0.001)  # mean and std
-    population = np.array([MLP(input_shape, mutation_parameters)
+    population = np.array([MLP(input_shape, 10)
                            for _ in range(len_of_population)])
     with torch.no_grad():
-        arr_of_nets = fit(population, epochs, loss_fn, train_dl, test_dl)
+        arr_of_nets = fit(population, epochs, mutation_parameters, loss_fn, train_dl, test_dl)
 
     fitted_net = arr_of_nets[0]
     x, y = test_dl.dataset.tensors[0], test_dl.dataset.tensors[1]
@@ -150,26 +162,38 @@ def main():
     accuracy = Accuracy().to(device)
     acc = accuracy(y_predict, y)
     print(f"Result accuracy={round(acc.item(), 3)}")
-    conf_mat = ConfusionMatrix(10).to(device)
-    conf_mat(y_predict, test_dl.dataset.tensors[1])
     return arr_of_nets
 
 
 if __name__ == "__main__":
     # https://towardsdatascience.com/evolving-neural-networks-b24517bb3701
+    pio.renderers.default = "browser"
+    softmax = torch.nn.Softmax(dim=-1)
     device = torch.device("cuda")
     # device = torch.device("cpu")
+    history_of_fittness = []
+    fig = make_subplots(rows=4, cols=3, subplot_titles=list(range(9)),
+                        specs=[[{}, {}, {}],
+                               [{}, {}, {}],
+                               [{}, {}, {}],
+                               [{"colspan": 3}, None, None]], )
     model = main()
 
-    fig, ax = plt.subplots(3, 3)
-    row, col = 0, 0
-    for i in range(9):
-        sns.heatmap(model[0].linear1.weight.detach().cpu().numpy()[i, :].reshape((28, 28)),
-                    ax=ax[row, col])
-        col += 1
-        if col == 3:
-            col = 0
-            row += 1
-    plt.title("END", fontsize=20)
-    plt.tight_layout()
-    plt.show()
+    steps = []
+    for i in range(len(history_of_fittness)):
+        step = dict(
+            method='restyle',
+            args=[{"visible": [False] * len(fig.data)}],
+        )
+        count_of_plots = 10
+        for k in range(count_of_plots):
+            step["args"][0]["visible"][count_of_plots * i + k] = True
+        steps.append(step)
+
+    sliders = [dict(
+        steps=steps,
+    )]
+
+    fig.layout.sliders = sliders
+
+    fig.show()
